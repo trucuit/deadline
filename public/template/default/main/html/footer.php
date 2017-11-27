97d9e0fd339b69a81d719435e4dae3efae0b63dd
huyhaonguyen <32436989+huyhaonguyen@users.noreply.github.com>
2017-11-27 23:57:00 +0700

    xong fe

--- public/template/default/main/html/footer.php
+++ public/template/default/main/html/footer.php
@@ -1,29 +1,96 @@
-<div id="footer">
-	
-<div id="footer" >
-	<div class="container">
-		<div class="row hr">
-			<div class="intro col-md-4">
-				<h4> Giới Thiệu</h4>
-				<img src="<?php echo $imageURL ?>/logo.png" class="logo-coupon" alt="mã coupon">
-				<p>Ma Coupon là nơi chia sẽ thông tin khuyến mãi, mã giảm giá khi mua hàng online tại các website mua sắm trực tuyến lớn và uy tín tại Việt Nam.
-				Phương châm hoạt động của Blog Mã Coupon mang đến cho bạn những thông tin mua sắm online An Toàn – Tiết Kiệm</p>
-			</div>	
-			<div class="lienket col-md-4">
-				<h4> Liên Kết</h4>
-				<div class="lk"><a href="#">Lazada</a></div>
-				<div class="lk"><a href="#">Shoppee</a></div>
-				<div class="lk"><a href="#">Tiki</a></div>
-				<div class="lk"><a href="#">Sendo</a></div>
-			</div>	
-			<div class="liennhe col-md-4">
-				<h4> Liên Hệ</h4>
-				<p>Email :abcd@gmail.com</p>
-			</div>	
-			
-		</div>
-	</div>
-	<div class="copyright text-center">
-		<span>Copyright © 2017 Blog Mã Coupon. All Rights Reserved. Quản lý và phát triển bởi AAAAAAAAA</span>
-	</div>
-</div>
+<!-- FOOTER -->
+<footer id="footer" class="footer">
+    <div class="first-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-3">
+                    <div class="widget megacourse">
+                        <h3 class="md">MegaCourse</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                        <a href="#" class="mc-btn btn-style-1">Get started</a>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="widget widget_latest_new">
+                        <h3 class="sm">Latest News</h3>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <div class="image-thumb">
+                                        <img src="public/template/default/main/images/team-13.jpg" alt="">
+                                    </div>
+                                    <span>How to have a good Boyfriend in college?</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <div class="image-thumb">
+                                        <img src="public/template/default/main/images/team-13.jpg" alt="">
+                                    </div>
+                                    <span>How to have a good Boyfriend in college?</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2">
+                    <div class="widget quick_link">
+                        <h3 class="sm">Quick Links</h3>
+                        <ul class="list-style-block">
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Terms of use</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Support</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="widget news_letter">
+                        <div class="awe-static bg-news_letter"></div>
+                        <div class="overlay-color-3"></div>
+                        <div class="inner">
+                            <div class="letter-heading">
+                                <h3 class="md">News letter</h3>
+                                <p>Don’t miss a course sale! Join our network today and keep it up!</p>
+                            </div>
+                            <div class="letter">
+                                <form>
+                                    <input class="input-text" type="text">
+                                    <span class="no-spam">* No spam guaranteed</span>
+                                    <input type="submit" value="Submit now" class="mc-btn btn-style-2">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="second-footer">
+        <div class="container">
+            <div class="contact">
+                <div class="email">
+                    <i class="icon md-email"></i>
+                    <a href="#"><span class="__cf_email__" data-cfemail="6a09051f18190f2a070f0d0b0e181f1a0b0644090507">[email&#160;protected]</span></a>
+                </div>
+                <div class="phone">
+                    <i class="fa fa-mobile"></i>
+                    <span>+84 989 999 888</span>
+                </div>
+                <div class="address">
+                    <i class="fa fa-map-marker"></i>
+                    <span>Maecenas sodales, nisl eget</span>
+                </div>
+            </div>
+            <p class="copyright">Copyright © 2014 Megadrupal. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+<!-- END / FOOTER -->
--- public/template/default/main/index.php
+++ public/template/default/main/index.php
@@ -1,36 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="icon" href="<?php echo  $this->_dirImg ?>/logon-ico.ico">
 
-	<?php
-	echo $this->_title;
-	echo $this->_metaHTTP;
-	echo $this->_metaName;
-	echo $this->_fileCSS;
-	echo $this->_fileJS;
+    <?php
+    echo $this->_title;
+    echo $this->_metaHTTP;
+    echo $this->_metaName;
+    echo $this->_fileCSS;
 
-	$imageURL = $this->_dirImg;
-	?>
+    $imageURL = $this->_dirImg;
+    ?>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,900' rel='stylesheet' type='text/css'>
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window,
+            document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '1031554816897182');
+        fbq('track', "PageView");
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+             src="https://www.facebook.com/tr?id=1031554816897182&ev=PageView&noscript=1"
+        />
+    </noscript>
+
+
 </head>
-<body>
-	<!--header-->
-	<?php include_once 'html/header.php' ?>
-	<!--//header-->
+<body id="page-top" class="home">
 
-	<!--banner-->
-	<?php 
-	if($this->_fileView =='index/index')
-		include_once 'html/banner.php' 
-	?>
-	<!--//banner-->
+<!-- PAGE WRAP -->
+<div id="page-wrap">
 
-	<!--content-->
-	<?php include_once MODULE_PATH . DS . $this->_moduleName . DS . 'views' . DS . $this->_fileView . '.php'; ?>
-	<!--//content-->
+    <!-- PRELOADER -->
+    <?php include_once 'html/preloader.php' ?>
+    <!-- END / PRELOADER -->
 
-	<!--footer-->
-	<?php include_once 'html/footer.php' ?>
-	<!--//footer-->
+    <!--header-->
+    <?php include_once 'html/header.php' ?>
+    <!--//header-->
+
+    <!--content-->
+    <?php include_once MODULE_PATH . DS . $this->_moduleName . DS . 'views' . DS . $this->_fileView . '.php'; ?>
+    <!--//content-->
+
+    <!--footer-->
+    <?php include_once 'html/footer.php' ?>
+    <!--//footer-->
+
+</div>
+<!-- END / PAGE WRAP -->
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-20585382-5', 'megadrupal.com');
+    ga('send', 'pageview');
+</script>
+<?php
+echo $this->_fileJS;
+?>
 </body>
 </html>
