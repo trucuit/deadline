a199cf4f65acc4c7387911502173a912db6a952f
huyhaonguyen <32436989+huyhaonguyen@users.noreply.github.com>
2017-12-01 13:53:09 +0700

    xong video liên quan

--- application/module/default/controllers/CoursesController.php
+++ application/module/default/controllers/CoursesController.php
@@ -14,16 +14,10 @@ class CoursesController extends Controller
 
     public function indexAction()
     {
+     $this->_view->video=$this->_model->videoQuery($this->_arrParam['id']);
 
+     $this->_view->category=$this->_model->videoRelativeQuery($this->_arrParam['id'],$this->_view->video[0]['name_category']);
 
-        $queryVideo="SELECT `c`.`name`AS `name_category`,`cs`.`name` AS `name_course`,`v`.`title`,`v`.`link`,`v`.`thumbnails`FROM `course` AS `cs` INNER JOIN `video`AS`v` ON `cs`.`id`=`v`.`course_id` 
-                                                                                           INNER JOIN `category` AS `c`ON `c`.`id`=`cs`.`category_id`
-                                                                                            WHERE `cs`.`id`=".$this->_arrParam['id'];
-        $query="SELECT DISTINCT `c`.`id`,`cs`.`name`,`a`.`name` AS `name_author` FROM `course`AS`cs` INNER JOIN `category`AS`c` ON `c`.`id`=`cs`.`category_id` 
-                                                                      INNER JOIN `author` AS`a` ON `a`.id =`cs`.`author_id`
-                                                                      WHERE `cs`.`id`!=".$this->_arrParam['id'];
-        $this->_view->category=$this->_model->execute($query,true);
-        $this->_view->video=$this->_model->execute($queryVideo,true);
         $this->_view->render('courses/index');
     }
 
