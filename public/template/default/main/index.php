835771049d2ea6df887db4c24eb5c00bcaef0eca
TRỰC TRẦN TRUNG <15520939@gm.uit.edu.vn>
2017-12-01 09:10:24 +0700

    icon loi

--- public/template/default/main/index.php
+++ public/template/default/main/index.php
@@ -6,7 +6,6 @@ $urlFile = TEMPLATE_URL . '/default/main';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?php echo $this->_title ?></title>
     <!-- Google font -->
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,900' rel='stylesheet' type='text/css'>
@@ -17,40 +16,16 @@ $urlFile = TEMPLATE_URL . '/default/main';
     echo $this->_metaName;
     echo $this->_fileCSS
     ?>
+    <title><?php echo $this->_title ?></title>
+
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
     <![endif]-->
-    <title>Mega Course - Learning and Courses HTML5 Template</title>
     <script type="text/javascript">
         root_url =  <?php echo json_encode(ROOT_URL . "/")  ?>;
         const ROOT_URL = root_url;
     </script>
-    <script>
-        !function (f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function () {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window,
-            document, 'script', '//connect.facebook.net/en_US/fbevents.js');
-
-        fbq('init', '1031554816897182');
-        fbq('track', "PageView");</script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=1031554816897182&ev=PageView&noscript=1"
-        /></noscript>
 </head>
 <body id="page-top" class="home">
 
@@ -104,21 +79,5 @@ $urlFile = TEMPLATE_URL . '/default/main';
 
 <!-- Load jQuery -->
 <?php echo $this->_fileJS ?>
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-20585382-5', 'megadrupal.com');
-    ga('send', 'pageview');
-</script>
 </body>
 </html>
