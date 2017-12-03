a4257fc21522c7f471739f6d816a43b1452c1c15
huyhaonguyen <32436989+huyhaonguyen@users.noreply.github.com>
2017-12-03 12:17:25 +0700

    update phần layout-vietsub

--- application/module/default/models/CoursesModel.php
+++ application/module/default/models/CoursesModel.php
@@ -9,19 +9,24 @@ class CoursesModel extends Model
 
     public function videoQuery($where)
     {
-        $queryVideo = "SELECT `c`.`name`AS `name_category`,`cs`.`name` AS `name_course`,`v`.`title`,`v`.`link`,`v`.`thumbnails`,`a`.`name` AS `name_author`FROM `course` AS `cs` INNER JOIN `video`AS`v` ON `cs`.`id`=`v`.`course_id` 
-                                                                                           INNER JOIN `category` AS `c`ON `c`.`id`=`cs`.`category_id`
-                                                                                           INNER JOIN `author`   AS  `a` ON`a`.`id`=`cs`.`author_id`
-                                                                                            WHERE `cs`.`id`=" . $where;
-        $video = $this->execute($queryVideo, true);
+        $queryVideo=array();
+        $queryVideo[] = "SELECT `c`.`name`AS `name_category`,`cs`.`name` AS `name_course`,`v`.`title`,`v`.`link`,`v`.`thumbnails`,`a`.`name` AS `name_author`";
+        $queryVideo[]="FROM `course` AS `cs` INNER JOIN `video`AS`v` ON `cs`.`id`=`v`.`course_id`";
+        $queryVideo[]="INNER JOIN `category` AS `c`ON `c`.`id`=`cs`.`category_id`";
+        $queryVideo[]="INNER JOIN `author`   AS  `a` ON`a`.`id`=`cs`.`author_id`";
+        $queryVideo[]= "WHERE `cs`.`id`=" . $where;
+        $video = $this->execute(implode(" ",$queryVideo), true);
         return $video;
     }
     public function videoRelativeQuery($id,$name)
     {
-        $query="SELECT DISTINCT `c`.`id`AS `category_id`,`cs`.`id` AS `course_id`,`cs`.`name` AS  `name_course`,`a`.`name` AS `name_author`,`c`.`name`AS `name_category`FROM `course`AS`cs` INNER JOIN `category`AS`c` ON `c`.`id`=`cs`.`category_id` 
-                                                                      INNER JOIN `author` AS`a` ON `a`.id =`cs`.`author_id`
-                                                                      WHERE `c`.`name`="."'".$name."' AND `cs`.`id`!=".$id;
-        $category=$this->execute($query,true);
+        $query=array();
+
+        $query[]="SELECT DISTINCT `c`.`id`AS `category_id`,`cs`.`id` AS `course_id`,`cs`.`name` AS  `name_course`,`a`.`name` AS `name_author`,`c`.`name`AS `name_category`";
+        $query[]="FROM `course`AS`cs` INNER JOIN `category`AS`c` ON `c`.`id`=`cs`.`category_id`";
+        $query[]="INNER JOIN `author` AS`a` ON `a`.id =`cs`.`author_id`";
+        $query[]="WHERE `c`.`name`="."'".$name."' AND `cs`.`id`!=".$id;
+        $category=$this->execute(implode(" ",$query),true);
         return $category;
     }
 
